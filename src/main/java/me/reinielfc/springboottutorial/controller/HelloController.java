2803ae2d7e75ba0d18ab22b8b7ad8a9a8f38fa82
Reiniel Fernandez <reinielfc@gmail.com>
2023-01-18 10:15:14 -0500

    09-RunningSpringBootApp

--- src/main/java/me/reinielfc/springboottutorial/controller/HelloController.java
+++ src/main/java/me/reinielfc/springboottutorial/controller/HelloController.java
@@ -1,12 +1,11 @@
 package me.reinielfc.springboottutorial.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String helloWorld() {
         return "Welcome to Daily Code Buffer!!";
     }
